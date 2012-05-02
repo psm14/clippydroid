97f1e2060cea455a3d5e60e6bc14977c8606abf4
Patrick McLaughlin <psm14case@gmail.com>
2012-05-02 13:51:39 -0400

    Initial

---
+++ src/com/clippy/assistant/ClippyDroidActivity.java
@@ -0,0 +1,13 @@
+package com.clippy.assistant;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ClippyDroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
